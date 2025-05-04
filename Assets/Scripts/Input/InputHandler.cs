dcffb7fee6224baf968a10523bafaf31df714683
GrosJambon1243 <kevindurocher@live.ca>
2025-05-04 15:35:22 -0400

    Implement pause functionality and update input actions
    Added a new "PauseMenu" action to the input actions configuration. Implemented pause and resume game logic in the InputHandler, utilizing the TimeManager for time control. Removed obsolete Test script and its associated meta file.

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -6,10 +6,13 @@ public class InputHandler : Singleton<InputHandler>
 {
     private GameManager gameManager;
     private Camera mainCamera;
+    private TimeManager timeManager;
+    private bool gamePaused;
 
     protected override void OnAwake()
     {
         gameManager = FindFirstObjectByType<GameManager>();
+        timeManager = FindFirstObjectByType<TimeManager>();
         mainCamera = Camera.main;
     }
 
@@ -30,5 +33,23 @@ public class InputHandler : Singleton<InputHandler>
         }
     }
 
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+
+        if (!gamePaused)
+        {
+            gamePaused = true;
+            timeManager.PauseTime();
+            Debug.Log("Pause Game");
+        }
+        else
+        {
+            gamePaused = false;
+            timeManager.ResumeTime();
+            Debug.Log("Resume Game");
+        }
+    }
+    
    
 }
